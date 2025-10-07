938775a899624f83d43c9ff88e7a12e833bcecb6
Shiran Mamistvalov <160395215+ShiranMamis@users.noreply.github.com>
2025-10-07 16:14:51 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        "medium-blue": "rgba(0,119,182,1)",
+      },
+    },
+  },
+  plugins: [],
+};
